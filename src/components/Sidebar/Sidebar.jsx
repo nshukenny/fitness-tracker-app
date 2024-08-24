07ae0f50573e987d36c6397a1011823cda46037c
Nshuti Kenny Herve <nshukenny@gmail.com>
2024-08-24 01:08:00 -0700

    dashboard fixed

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -9,27 +9,49 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import ComputerIcon from '@mui/icons-material/Computer';
 import PersonIcon from '@mui/icons-material/Person';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+
+const menuItems = [
+  {
+    text: 'Dashboard',
+    icon: <DashboardIcon sx={{ color: '#2196f3' }} />,
+    link: '/dashboard/home',
+  },
+  {
+    text: 'Users',
+    icon: <PersonIcon sx={{ color: '#ff5722' }} />,
+    link: '/dashboard/users',
+  },
+  {
+    text: 'Work Sessions',
+    icon: <ComputerIcon sx={{ color: '#2196f3' }} />,
+    // link: '/dashboard/work-sessions',
+  },
+];
 
 export default function Sidebar() {
-  const list = (
-    <Box
+  return (
+    <Drawer
+      anchor="left"
+      open={true}
+      variant="persistent"
       sx={{
-        width: 200,
-        paddingTop: 20,
+        '& .MuiDrawer-paper': {
+          width: 220,
+          boxSizing: 'border-box',
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          height: '100%',
+        },
       }}
-      role="presentation"
     >
-      <List>
-        {['Dashboard', 'Users', 'Work Sessions'].map((text, index) => (
-          <ListItem
-            key={text}
-            disablePadding
-            sx={{ marginTop: index === 0 ? 0 : 0 }}
-          >
-            {' '}
-            {text === 'Users' ? (
+      <Box sx={{ width: 200, paddingTop: 20 }} role="presentation">
+        <List>
+          {menuItems.map(({ text, icon, link }) => (
+            <ListItem key={text} disablePadding>
               <Link
-                to="/dashboard/users"
+                to={link}
                 style={{
                   textDecoration: 'none',
                   color: 'inherit',
@@ -39,55 +61,18 @@ export default function Sidebar() {
                 <ListItemButton
                   sx={{
                     width: '100%',
-                    '&:hover': {
-                      backgroundColor: '#f0f0f0',
-                    },
+                    '&:hover': { backgroundColor: '#f0f0f0' },
                   }}
                 >
-                  <ListItemIcon>
-                    <PersonIcon sx={{ color: '#ff5722' }} />
-                  </ListItemIcon>
+                  <ListItemIcon>{icon}</ListItemIcon>
                   <ListItemText primary={text} />
                 </ListItemButton>
               </Link>
-            ) : (
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? (
-                    <ComputerIcon sx={{ color: '#2196f3' }} />
-                  ) : (
-                    <PersonIcon sx={{ color: '#ff5722' }} />
-                  )}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            )}
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-    </Box>
-  );
-
-  return (
-    <div>
-      <Drawer
-        anchor="left"
-        open={true}
-        variant="persistent"
-        sx={{
-          '& .MuiDrawer-paper': {
-            width: 220,
-            boxSizing: 'border-box',
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            height: '100%',
-          },
-        }}
-      >
-        {list}
-      </Drawer>
-    </div>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+      </Box>
+    </Drawer>
   );
 }
